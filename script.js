0aa7fa33200e6ea53c69d9a47cb3deef8923c8f1
Buharova Elizaveta <byharova.077@gmail.com>
2025-02-10 21:58:59 +0300

    end event listener

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ myButton.innerHTML = "Нажми меня";
 myButton.addEventListener('mouseover', () => myButton.style.background = 'red');
 myButton.addEventListener('mouseout', () => myButton.style.background = "grey");
 
+let textInput = document.getElementById('textInput');
+let displayText = document.getElementById('displayText');
+textInput.addEventListener('input', () => {displayText.textContent = textInput.value});
+
+let myForm = document.getElementById('myForm');
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.querySelector('input[name="username"]').value;
+    console.log(name)});
